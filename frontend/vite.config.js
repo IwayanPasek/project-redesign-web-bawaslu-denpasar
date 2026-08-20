8631693fce6a249b5988b4b7ab9bb391fcfb13eb
I WAYAN PASEK GEDE SEGARA CAYMAN <iwayanpasek23@gmail.com>
2026-08-20 10:34:17 +0800

    fix: deploy ke github pages (base path & homepage url)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react' // (Contoh jika menggunakan React)
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/project-redesign-web-bawaslu-denpasar/',
   plugins: [
     tailwindcss(),
     react(),
